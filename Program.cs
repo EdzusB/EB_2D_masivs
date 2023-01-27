56178f82562658b06226d1e3a44f8ae33c497451
EdzusB <edzusbleikss1@gmail.com>
2023-01-27 08:47:14 +0200

    Pirma projekta lejupielade

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EB_2D_istais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
